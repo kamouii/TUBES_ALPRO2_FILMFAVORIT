5cb4dd47da398d870ad48ba12ef34331b47684d7
kamouii <mdanielamsyar02@gmail.com>
2026-06-05 23:04:18 +0700

    Update

--- tubes.go
+++ tubes.go
@@ -1,9 +1,14 @@
 package main
+
 import "fmt"
-func main () {
-    var n,i int
-    fmt.Scan(&n)
-    for i = 0; i < n; i++ {
-        fmt.Println("Hello World")
-    }
+
+func main() {
+	var n, i int
+	fmt.Scan(&n)
+	
+	for i = 0; i < n; i++ {
+		fmt.Println("Daniel")
+	}
+
 }
+
